63f4faef4216d17c3ad118e4027fd988a14d68e0
junjie635 <1295448630@qq.com>
2024-04-16 00:15:11 +0800

    fix

--- src/background/service-worker.ts
+++ src/background/service-worker.ts
@@ -1,1 +1,18 @@
 console.log("service-worker");
+let db;
+chrome.runtime.onInstalled.addListener(() => {
+    const request = indexedDB.open("db")
+    request.onerror = function () {
+        console.error("Error opening IndexedDB database.");
+    };
+    request.onsuccess = function (event) {
+        const target: IDBOpenDBRequest = event.target as IDBOpenDBRequest;
+        db = target?.result
+        console.log("indexedDB loaded successfully");
+        const keys = db.createObjectStore("keywords", { keyPath: "name" })
+    };
+    request.onupgradeneeded = function () {
+        console.log("upgradeneeded");
+    };
+
+})
